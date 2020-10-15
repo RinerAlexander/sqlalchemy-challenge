eb518e86c08d21681ff1df478860139ab93654bf
Alex Riner <rineralexander@gmail.com>
2020-10-15 12:05:14 -0500

    station and tobs working, percipitation updated to only return past year

--- app.py
+++ app.py
@@ -34,8 +34,7 @@ def welcome():
 def precipitation():
 
     session = Session(engine)
-    results = session.query(meas.date,meas.prcp).all()
-
+    results = session.query(meas.date,meas.prcp).filter(meas.date>="2016-08-23").all()
     session.close()
 
     pecipDict={}
@@ -43,6 +42,31 @@ def precipitation():
          pecipDict[measurments.date]=measurments.prcp
 
     return jsonify(pecipDict)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    results = session.query(stat.station).all()
+    session.close()
+
+    statList=[]
+    for station in results:
+        statList.append(station[0])
+    
+    return jsonify(statList)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+
+    session = Session(engine)
+    results = session.query(meas.date,meas.tobs).filter(meas.date>="2016-08-23",meas.station=="USC00519281").all()
+    session.close()
+
+    tobsDict={}
+    for measurments in results:
+         tobsDict[measurments.date]=measurments.tobs
+
+    return jsonify(tobsDict)
 
 
 
